8e3ccff66ac10c1296d653f6bcabffeced17aebe
Evelyn_Charlesworth <charlesworth.evelyn@gmail.com>
2022-09-05 12:51:40 +1200

    starting task for Matt

--- model_build/supporting_data_analysis/all_wells.py
+++ model_build/supporting_data_analysis/all_wells.py
@@ -3,6 +3,7 @@ created matt_dumont
 on: 5/09/22
 """
 import pandas as pd
+import numpy as np
 from model_build.project_model_tools import smt
 from project_base import processed_model_build_data_dir, base_model_build_data_dir, modelling_dir
 import rasterio
@@ -21,14 +22,24 @@ def get_all_wells(recalc=False):
     well_data.set_index('well_name', inplace=True)
     well_data.dropna(subset='Depth', inplace=True)
 
+
     # DO NOT USE A FOR LOOP! # todo EC
 
-    # todo set qual code 1 for no elv data, 2 for with elv data
+    # setting up quality code for elevation data
+    # 1 = no elv data, 2 = elv data
+    idx = well_data["Elevation"].isnull()
+    well_data.loc[:, 'quality_code'] = 2
+    well_data.loc[idx, 'quality_code'] = 1
 
     # todo get elevation from DEM where missing
+
+    # missing elevation data is where quality_code = 1, e.g same as idx
     # use rasterio and raster.sample
     raster = rasterio.open(dem_path)
-    raster.sample()
+    xy = zip(well_data.loc[idx, 'EastingTM'], well_data.loc[idx, 'NorthingTM'])
+    t = raster.sample(xy)
+    t = np.array(list(t)).flatten()
+    well_data.loc[idx, 'Elevation'] = t
 
     # todo convert depth to elevation (use ground RL if present)
 
@@ -41,7 +52,10 @@ def get_all_wells(recalc=False):
     # todo correctly format depth to water and drill_date
 
     # todo calculate layer, row, col
+    # leave this
     smt.convert_coords_to_matix()
 
     # todo save to:  as csv
     processed_well_path
+
+get_all_wells()
