fix some plots fix river targets (sign inversed) add riv targets to all riv plots

optimisation/hawea_plot_optimisation.py
@@ -12,7 +12,7 @@ from targets_and_sensitive_sites.model_output import process_model_output
 from optimisation.model_utils_for_forward_run import read_param_data, build_run_model
 from model_tools.plot_optimisation import plot_optimisation_and_extract_info
 from model_parameterisation.inital_parametersiation import *
-from model_parameterisation.pilot_points import interpolate_sy_pilot_points,   interpolate_kh_pilot_points
+from model_parameterisation.pilot_points import interpolate_sy_pilot_points, interpolate_kh_pilot_points
 from model_tools.util_functions.list_file_utils import ListSolverInfo
 import py7zr
 
@@ -88,7 +88,11 @@ def plot_opt(pest_dir, replot=False, plot_failure_points=True):
         for k, (start, (upper, lower)) in init.items():
             imesure = meas[k]
             out.loc[k, 'group'] = pref
-            out.loc[k, 'norm_param_val'] = (imesure - lower) / (upper - lower)
+            if pref in ['riv', 'kh']:
+                out.loc[k, 'norm_param_val'] = ((np.log10(imesure) - np.log10(lower))
+                                                / (np.log10(upper) - np.log10(lower)))
+            else:
+                out.loc[k, 'norm_param_val'] = (imesure - lower) / (upper - lower)
     with open(base_plot_dir.joinpath('parameters_norm_to_bounds.txt'), 'w') as f:
         f.write(out.to_string())
     out = out.loc[(out.norm_param_val >= 0.9) | (out.norm_param_val <= 0.1)]
@@ -103,7 +107,11 @@ def plot_opt(pest_dir, replot=False, plot_failure_points=True):
         meas = eval(f'{pref}_param')
         for k, (start, (upper, lower)) in init.items():
             imesure = meas[k]
-            norm_val = (imesure - lower) / (upper - lower)
+            if pref == 'kh':
+                norm_val = ((np.log10(imesure) - np.log10(lower))
+                            / (np.log10(upper) - np.log10(lower)))
+            else:
+                norm_val = (imesure - lower) / (upper - lower)
             plocs.loc[k, pref] = norm_val
 
     fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(10, 8))
@@ -146,7 +154,6 @@ def plot_opt(pest_dir, replot=False, plot_failure_points=True):
 
         with open(base_plot_dir.joinpath('num_outer_itts.txt'), 'w') as f:
             f.write(t.to_string())
-
 
 
 def _dummy(x):

targets_and_sensitive_sites/model_output.py
@@ -133,17 +133,21 @@ def _plot_spatial_heads(all_hds, ibound, plot_dir, dry_hds, flooded_cells):
     plt.close(fig)
 
 
-def _plot_all_riv_obs(all_riv_obs, plot_dir, riv_keys, riv_colors):
+def _plot_all_riv_obs(all_riv_obs, plot_dir, riv_keys, riv_colors, out_obs):
     print(myself())
     # plot all river observations
     fig, axs = plt.subplots(4, sharex=True, figsize=(12, 9))
     for ax, k, c in zip(axs, riv_keys, riv_colors):
         ax.plot(all_riv_obs.index, all_riv_obs.loc[:, k], color=c, marker='.', label=k)
+        temp = out_obs.loc[(out_obs.name.str.contains(k)) & (out_obs.group == 'riv')]
+        ax.scatter(temp.nper, temp.measured, color=c, label=f'{k.capitalize()} target',
+                   marker="X"
+                          "")
         ax.axhline(0, ls=':', color='k')
         ax.set_yscale('symlog')
         ax.legend()
     fig.supxlabel('Period')
-    fig.supylabel('River Gain/loss(-)')
+    fig.supylabel('River Gain(+)/loss(-)')
     fig.tight_layout()
     fig.savefig(plot_dir.joinpath('all_river_fluxes.png'))
     plt.close(fig)
@@ -328,7 +332,7 @@ def _plot_riv_obs(riv_keys, riv_colors, out_obs, plot_dir):
     plot_1_to_1(ax, ls=':', c='k', label='1:1 line')
     ax.legend()
     fig.tight_layout()
-    fig.savefig(plot_dir.joinpath('all_riv_targets.png'))
+    fig.savefig(plot_dir.joinpath('all_riv_targets_mes_mod.png'))
     plt.close(fig)
 
     # riv obs by time
@@ -344,7 +348,7 @@ def _plot_riv_obs(riv_keys, riv_colors, out_obs, plot_dir):
     fig.supxlabel('Model period')
     fig.supylabel('Residual (modelled - measured, m3/day)')
     fig.tight_layout()
-    fig.savefig(plot_dir.joinpath('all_riv_targets.png'))
+    fig.savefig(plot_dir.joinpath('all_riv_targets_residual.png'))
     plt.close(fig)
 
 
@@ -420,10 +424,11 @@ def _plot_budget(list_file, plot_dir, plot_transient_budget):
             plt.close(fig)
 
 
-def _plot_spatial_riv(all_riv, plot_dir):  # todo
-    assert isinstance(plot_dir, Path)
-    outdir = plot_dir.joinpath('spatial_riv')
-    outdir.mkdir(exist_ok=True)
+def _plot_spatial_riv(all_riv, plot_dir):
+    if plot_dir is not None:
+        assert isinstance(plot_dir, Path)
+        outdir = plot_dir.joinpath('spatial_riv')
+        outdir.mkdir(exist_ok=True)
     to_plots = {
         'min': np.nanmin(all_riv, axis=0),
         'mean': np.nanmean(all_riv, axis=0),
@@ -434,12 +439,16 @@ def _plot_spatial_riv(all_riv, plot_dir):  # todo
     for p in [5, 25, 50, 75, 95]:
         to_plots[f'{p:02d} percentile'] = np.nanpercentile(all_riv, p, axis=0)
 
+    discriptor = '\n- is gaining stream (water out of model)\n+ is losing stream (water into model)'
     for k, to_plot in to_plots.items():
         norm = SymLogNorm(100, 1, vmin=np.nanmin(to_plot), vmax=np.nanmax(to_plot))
-        fig, ax = smt.plot.plt_matrix(to_plot, base_map=True, alpha=1, cmap='RdBu', norm=norm, title=k)
+        fig, ax = smt.plot.plt_matrix(to_plot, base_map=True, alpha=1, cmap='RdBu', norm=norm, title=k + discriptor)
         fig.tight_layout()
-        fig.savefig(outdir.joinpath(f'riv_{k.replace(" ", "_")}.png'))
-        plt.close(fig)
+        if plot_dir is not None:
+            fig.savefig(outdir.joinpath(f'riv_{k.replace(" ", "_")}.png'))
+            plt.close(fig)
+    if plot_dir is None:
+        plt.show()
 
 
 def visualise_model(model_ws, all_hds, dry_hds, out_obs, all_riv_obs, flooded_cells, all_riv, list_file,
@@ -484,7 +493,7 @@ def visualise_model(model_ws, all_hds, dry_hds, out_obs, all_riv_obs, flooded_ce
 
     _plot_spatial_heads(all_hds, ibound, plot_dir, dry_hds, flooded_cells)
 
-    _plot_all_riv_obs(all_riv_obs, plot_dir, riv_keys, riv_colors)
+    _plot_all_riv_obs(all_riv_obs, plot_dir, riv_keys, riv_colors, out_obs)
 
     _plot_hds_modelled_v_measured(hds_groups, hds_colors, hds_obs, plot_dir, regular_wells, regular_colors, zones,
                                   zcolors)
@@ -563,7 +572,7 @@ def process_model_output(model_ws, hds_file, plot=False, savelist=True, save_par
 
 if __name__ == '__main__':
     t = time.time()
-    test_hds = Path('/home/matt_dumont/unbacked/hawea/structure_v4/init_structure_v4/base_model/opt_model.hds')
+    test_hds = Path('/home/matt_dumont/unbacked/hawea/structure_v5/init_structure_v5/base_model/opt_model.hds')
     process_model_output(test_hds.parent,
                          test_hds,
                          plot=True)