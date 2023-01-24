caed33d6beaa0c6586d51590fb67a446480ab0ca
Matt_Dumont <hansonmcoombs@gmail.com>
2023-01-25 08:47:58 +1300

    try lowering conductivity

--- optimisation/manual_optimisations/from_pest_run.py
+++ optimisation/manual_optimisations/from_pest_run.py
@@ -133,12 +133,50 @@ def large_run():
                        safemode=False, rm_remote_files=True)
 
 
+def try_shift_hflat():
+    print_myself()
+    opt_name = 'large_run'
+    base_params = read_param_data(
+        parameter_file='/home/matt_dumont/unbacked/hawea/3d_v1a/init2_3d_v1a/Optimisations/opt.par',
+        format='pest',
+        return_individual=False
+
+    )
+
+    new_params = []
+    lowers = [5, 50]
+    uppers = [500, 200]
+    peak = [300, 500, 700, 1000]
+    moraine_vals = [200, 400, 600]
+    sy_vals = [.1]
+    for m, sy, l, u, pk in itertools.product(moraine_vals, sy_vals, lowers, uppers, peak):
+        temp = {}
+        temp['kh_mor_l0'] = m
+        temp['kh_h_flat5'] = l
+        temp['kh_h_flat46'] = l
+        temp['kh_h_flat89'] = u
+        hflats = [4, 10, 3, 45, 8, 7, 46, 5, 89, 2, 1, 51, 9]
+        for hflat in hflats:
+            temp[f'sy_h_flat{hflat}'] = sy
+
+        hflats2 = [3, 45, 8, 7, 6, 51]
+        for hflat in hflats2:
+            temp[f'kh_h_flat{hflat}'] = pk
+
+        new_params.append(temp)
+    opt_name = f'{branch}_{opt_name}'
+    run_simple_man_opt(name=opt_name, mod_params=new_params, base_param_dict=base_params, replot=replot,
+                       safemode=False, rm_remote_files=True)
+
+
 # todo play with morain l0 kh, play with rasing kh above 1000, play with setting sy to higher values, remove ngmp wells as targets
 #  re-run opt from "manual" param file.
 
+
 replot = False
 if __name__ == '__main__':
-    large_run()
+    try_shift_hflat()
+    # large_run()
     # moraine_sy()
     # check_moraine()
     # kh_test()
