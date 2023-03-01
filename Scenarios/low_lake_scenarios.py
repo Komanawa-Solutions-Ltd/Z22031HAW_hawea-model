2bf9440f86303d51928b6c0aabcd99ddc5e0cf93
Matt_Dumont <hansonmcoombs@gmail.com>
2023-03-01 14:26:30 +1300

    new low lake scenarios

--- Scenarios/low_lake_scenarios.py
+++ Scenarios/low_lake_scenarios.py
@@ -15,21 +15,22 @@ import inspect
 import numpy as np
 from Scenarios.low_lake_scenario_data import get_low_lake_params, mod_sin_asym
 import traceback
-from model_build.project_model_tools import bund_top
+from model_build.project_model_tools import bund_top, l2_top
 
 
 def get_lake_hds(key):
     xs = np.arange(1, 52 + 1)
     base_a, base_b, base_d = get_low_lake_params()
+
     possible_data = {
         'base': mod_sin_asym(xs, d=base_d,
                              low_a=base_a, low_b=base_b, low_k=1,
                              high_a=base_a, high_b=base_b, high_k=1),
 
         # widening the low
-        'low_wide_0.9': mod_sin_asym(xs, d=base_d,
-                                     low_a=base_a, low_b=base_b, low_k=.75,
-                                     high_a=base_a, high_b=base_b, high_k=1),
+        'low_wide_0.75': mod_sin_asym(xs, d=base_d,
+                                      low_a=base_a, low_b=base_b, low_k=.75,
+                                      high_a=base_a, high_b=base_b, high_k=1),
         'low_wide_0.5': mod_sin_asym(xs, d=base_d,
                                      low_a=base_a, low_b=base_b, low_k=.5,
                                      high_a=base_a, high_b=base_b, high_k=1),
@@ -62,6 +63,12 @@ def get_lake_hds(key):
         'shift_-3.5': mod_sin_asym(xs, d=base_d,
                                    low_a=base_a, low_b=base_b - 3.5, low_k=1,
                                    high_a=base_a, high_b=base_b - 3.5, high_k=1),
+        'shift_-5': mod_sin_asym(xs, d=base_d,
+                                 low_a=base_a, low_b=base_b - 5, low_k=1,
+                                 high_a=base_a, high_b=base_b - 5, high_k=1),
+        'shift_-7': mod_sin_asym(xs, d=base_d,
+                                 low_a=base_a, low_b=base_b - 7, low_k=1,
+                                 high_a=base_a, high_b=base_b - 7, high_k=1),
 
         # amplitude
         'low_amp_1.25': mod_sin_asym(xs, d=base_d,
@@ -89,9 +96,23 @@ def get_lake_hds(key):
                                   low_a=base_a * 4, low_b=base_b, low_k=1,
                                   high_a=base_a, high_b=base_b, high_k=1),
 
-        # todo others ??? after running these
+        'low_wid_amp_0.5_4': mod_sin_asym(xs, d=base_d,
+                                          low_a=base_a * 3, low_b=base_b - 3, low_k=.5,
+                                          high_a=base_a, high_b=base_b - 3, high_k=1),
+        'low_wid_amp_0.75_4': mod_sin_asym(xs, d=base_d,
+                                           low_a=base_a * 3, low_b=base_b - 3, low_k=.75,
+                                           high_a=base_a, high_b=base_b - 3, high_k=1),
+        'low_wid_amp_0.9_4': mod_sin_asym(xs, d=base_d,
+                                          low_a=base_a * 3, low_b=base_b - 3, low_k=.9,
+                                          high_a=base_a, high_b=base_b - 3, high_k=1),
 
     }
+    t = [[np.mean(possible_data['base'])]] + [possible_data['low_amp_4'][0:np.argmin(possible_data['low_amp_4'])]]
+    t = np.concatenate(t)
+    t2 = np.concatenate((t, np.zeros((52 * low_lake_years + 1) - len(t)) + 333))
+    possible_data['lake_drop_333'] = t2
+    t2 = np.concatenate((t, np.zeros((52 * low_lake_years + 1) - len(t)) + 320))
+    possible_data['lake_drop_320'] = t2
 
     if key == 'all':
         return xs, possible_data
@@ -99,7 +120,7 @@ def get_lake_hds(key):
         return xs, possible_data[key]
 
 
-low_lake_groups = ('low_wide', 'shift', 'low_amp')
+low_lake_groups = ('low_wide', 'shift', 'low_amp', 'low_wid_amp', 'lake_drop')
 
 
 def plot_possible_hds(save=False):
@@ -114,6 +135,8 @@ def plot_possible_hds(save=False):
 
         for c, k in zip(colors, use_keys):
             v = data[k]
+            if 'lake_drop' in k:
+                xs = range(len(v))
             ax.plot(xs, v, color=c, label=k)
         ax.set_xlabel('ISO week')
         ax.set_ylabel('lake head')
@@ -122,7 +145,8 @@ def plot_possible_hds(save=False):
         fig.tight_layout()
         if save:
             fig.savefig(save_path)
-    plt.show()
+    if not save:
+        plt.show()
 
 
 def print_myself(key):
@@ -168,13 +192,25 @@ def run_low_lake_scenario(key, base_run_dir, base_outdir, build_run_model=True, 
                                                                 hill_param, race_param, False), mismatch_keys='warn')
     lake = low_lake_tdis.make_spd_steady(get_scen_ghb_data(scen_tdis), mismatch_keys='warn')
     xs, usehds = get_lake_hds(key)
-    usehds = np.concatenate([[usehds.mean()]] +
-                            [usehds for e in range(low_lake_years)]
-                            )
+    if len(usehds) == 52:
+        usehds = np.concatenate([[usehds.mean()]] +
+                                [usehds for e in range(low_lake_years)]
+                                )
+    elif len(usehds) == 52 * low_lake_years + 1:
+        pass
+    else:
+        raise NotImplementedError
     for p, lh in enumerate(usehds):
-        if lh < bund_top:
+        if lh < l2_top:
+            temp = lake[p]
+            temp = temp[temp['k'] > 1]
+            temp['bhead'] = lh
+            lake[p] = temp
+
+        elif lh < bund_top:
             temp = lake[p]
             temp = temp[temp['k'] > 0]
+            temp['bhead'] = lh
             lake[p] = temp
         else:
             lake[p]['bhead'] = lh
@@ -195,7 +231,7 @@ def run_low_lake_scenario(key, base_run_dir, base_outdir, build_run_model=True, 
                  well_spd=wel_data,
                  outdir=base_outdir.joinpath(key),
                  build_run_model=build_run_model, process_results=process_results,
-                 tickper=4)
+                 tickper=8)
 
 
 def try_run_all(rerun=False, subset=None):
@@ -225,4 +261,5 @@ def try_run_all(rerun=False, subset=None):
 
 
 if __name__ == '__main__':
-    try_run_all(rerun=True)
+    plot_possible_hds(True)
+    try_run_all(rerun=False)
