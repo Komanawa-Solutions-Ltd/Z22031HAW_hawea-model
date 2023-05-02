67000b3fac2896557274af46fb260712718acb9e
Matt_Dumont <hansonmcoombs@gmail.com>
2023-05-02 16:26:41 +1200

    ADD FULL SCEN LIST

--- Scenarios/run_grid_allocation.py
+++ Scenarios/run_grid_allocation.py
@@ -4,6 +4,8 @@ on: 8/03/23
 """
 import sys
 
+import pandas as pd
+
 sys.path.append('/home/matt_dumont/PycharmProjects/Z22031HAW_hawea-model')
 sys.path.append('/home/matt_dumont/PycharmProjects/modflow_tools_haw')
 from Scenarios.allocation_scenarios import run_all_grid_allocation_scens, zones_to_model, run_grid_allocation_scenario, \
@@ -46,7 +48,7 @@ def how_long_per_run():
     external_ips = [
         '170.64.182.254'
     ]
-    from run_managers.ssh_distributor import SshDist # keynote private repo
+    from run_managers.ssh_distributor import SshDist  # keynote private repo
     branch = 'main'
     local_cores = 4
     base_paths = {'100.124.148.71': unbacked_dir.joinpath('grid_scenarios'),
@@ -171,6 +173,7 @@ def main_grid_allov3(test=False, print_runs_only=False):
                                   pump_rate=runs, rm_remote_files=False, pers=pers, external_ips=external_ips
                                   )
 
+
 def main_grid_allo_riv_terrace(test=False, print_runs_only=False):
     local_cores = 4
     external_ips = ['170.64.184.50']
@@ -180,7 +183,7 @@ def main_grid_allo_riv_terrace(test=False, print_runs_only=False):
     max_allo = max_allo.to_dict()
     base_runs = {
         # fraction increase from current max allocation
-        'Terrace-River': [0.1, 0.25, 0.5, 1, 1.5, 1.75], # no more runs needed
+        'Terrace-River': [0.1, 0.25, 0.5, 1, 1.5, 1.75],  # no more runs needed
     }
     num_runs = 0
     for zone, pump_increases in base_runs.items():
@@ -205,10 +208,62 @@ def main_grid_allo_riv_terrace(test=False, print_runs_only=False):
                                   )
 
 
+def print_all_allo_scens():
+    all_runs = {}
+    base_runs = {
+        # fraction increase from current max allocation
+        'Terrace-River': [0.1, 0.25, 0.5, 1, 1.5],
+        'Terrace-Hill': [0.1, 0.25, 0.5, 1, 1.5],
+        # 'Mangawera Valley': [],  No runs needed, current allocation could cause problems
+        'Hawea Flat': [0.05, 0.1, 0.2, 0.3, 0.5, .75, 1, 1.5],
+
+        # no useage/allocation presently (values of pumping to add)
+        'Te Awa': [500, 1000, 2500, 5000, 7500, 10000],
+        'Maungawera Flat': [500, 1000, 2500, 5000, 7500, 10000],
+    }
+    base_runs2_3 = {
+        # fraction increase from current max allocation
+        'Terrace-River': [0.1, 0.25, 0.5, 1, 1.5, 1.75],  # no more runs needed
+
+        'Terrace-Hill': [2, 2.5, 5, 7.5, 10],
+
+        # no useage/allocation presently (values of pumping to add)
+        'Te Awa': [15000, 20000, 25000, 50000],
+        'Maungawera Flat': [15000, 20000, 30000], }
+
+    max_allo = get_pumping_in_zones().loc[:, 'max_allo_min'].abs()
+    max_allo.loc[max_allo.isna()] = 1
+    max_allo = max_allo.to_dict()
+    runs = pd.DataFrame()
+    i = 0
+    for zone, pump_increases in base_runs.items():
+        ma = max_allo[zone]
+        for pinc in pump_increases:
+            runs.loc[i, 'zone'] = zone
+            if ma > 1:
+                runs.loc[i, 'percent_increase'] = pinc
+            runs.loc[i, 'pumping_increase'] = rate = round(ma * pinc)
+            runs.loc[i, 'name'] = f'{zone} MAPC + {rate} $m^3/day$'
+            i += 1
+
+    reductions = [0.95, 0.9, 0.85, 0.8, 0.7, 0.6, 0.5]
+    for r in reductions:
+        runs.loc[i, 'zone'] = 'Maungawera Valley'
+        runs.loc[i, 'pumping_increase'] = -1 * (1 - r) * max_allo['Mangawera Valley']
+        runs.loc[i, 'percent_increase'] = -1 * (1 - r)
+        runs.loc[i, 'name'] = f'reduction_{r}'
+        i += 1
+    from project_base import proj_root
+    runs = runs.sort_values(['zone', 'pumping_increase'])
+    runs[['name', 'zone', 'pumping_increase', 'percent_increase']].to_csv(
+        proj_root.joinpath('support_figures', 'allo_scens.csv'), index=False)
+
+
 if __name__ == '__main__':
-    main_grid_allo_riv_terrace(print_runs_only=False)
+    print_all_allo_scens()
+    # main_grid_allo_riv_terrace(print_runs_only=False)
     # how_long_per_run()
-    #main_grid_allov3(test=False)
+    # main_grid_allov3(test=False)
     # test_indvidual()
     # test_ind_mp()
     # test_grid_allo()
