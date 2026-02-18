dd55d35a5ea8d26dacb0549fd1fdf7a24cd5e304
Matt <hansonmcoombs@gmail.com>
2026-02-19 12:11:55 +1300

    start refactoring to pipable install address internal imports

---
+++ src/komanawa/hawea/dummy_packages/__init__.py
@@ -0,0 +1,9 @@
+"""
+created matt_dumont 
+on: 13/04/23
+"""
+from komanawa.hawea.dummy_packages.regular_modeltools import DummyModelTools_RegularGrid as ModelTools_RegularGrid
+from komanawa.hawea.dummy_packages.time_discretization import DummyTimeDis as TimeDis
+
+
+
