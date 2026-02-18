6946fd110f62143002450f17abc58731ebc3b94f
Matt <hansonmcoombs@gmail.com>
2026-02-19 12:39:23 +1300

    start refactoring to pipable install address internal imports - transform opt.sdv to opt.sdv.7z

--- optimisation/optimisation_results/3d_v1b/0_copy_all_data_from_remotes.sh
+++
@@ -1,1 +0,0 @@
-scp -r matt_dumont@100.121.150.68:/media/matt_dumont/data/mh_unbacked/hawea/3d_v1b/init_3d_v1b/Optimisations/* /home/matt_dumont/unbacked/hawea/3d_v1b/init_3d_v1b/Optimisations/tuke_remote_files
--- optimisation/optimisation_results/3d_v1b/0_run_pest_checks.sh
+++
@@ -1,3 +0,0 @@
-pestchek opt |& tee 0_pest_check_results
-inschek observations.dat.ins observations.dat |& tee -a 0_pest_check_results
-tempchek parameters.dat.tpl parameters.dat.tpl.tempcheck.dat trial.par |& tee -a /home/matt_dumont/unbacked/hawea/3d_v1b/init_3d_v1b/Optimisations/0_pest_check_results
